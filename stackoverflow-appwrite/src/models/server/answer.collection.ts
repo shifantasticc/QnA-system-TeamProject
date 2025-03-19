2dc550d06a480b786258418d274cc994f8139821
Fareen <fareenhayat0308@gmail.com>
2025-03-19 12:27:34 +0530

    Updated necessary code files

---
+++ stackoverflow-appwrite/src/models/server/answer.collection.ts
@@ -0,0 +1,23 @@
+import{IndexType, Permission} from "node-appwrite" 
+import {answerCollection, db} from "../name" 
+import {databases} from "./config" 
+
+export default async function createAnswerCollection() { 
+    //create collection 
+    await databases.createCollection(db,answerCollection,answerCollection,[Permission.read("any"),Permission.read("users"),
+        Permission.create("users"),
+        Permission.update("users"),
+        Permission.delete("users"),
+
+    ])
+    console.log("Answer Collection is created");  
+     //Creating attributes and indexes 
+        await Promise.all([ 
+            databases.createStringAttribute(db,answerCollection,"content",10000,true), 
+            
+            databases.createStringAttribute(db,answerCollection,"questionid",50,true),
+            databases.createStringAttribute(db,answerCollection,"authorId",50,true),
+    
+        ]); 
+        console.log("Answer Attribute created"); 
+    }
---
+++ stackoverflow-appwrite/src/models/server/vote.collection.ts
@@ -0,0 +1,27 @@
+import{Permission} from "node-appwrite" 
+import {db,voteCollection} from "../name" 
+import {databases} from "./config"  
+export default async function createVoteCollection() { 
+    //create collection 
+    await databases.createCollection(db,voteCollection,voteCollection,[Permission.read("any"),Permission.read("users"),
+        Permission.create("users"),
+        Permission.update("users"),
+        Permission.delete("users"),
+
+    ]); 
+    console.log("Vote Collection is created");  
+         //Creating attributes and indexes 
+            await Promise.all([ 
+                databases.createEnumAttribute(db,voteCollection,"type",["answer","question"],true),  
+                
+                databases.createStringAttribute(db,voteCollection,"typeid",50,true),
+                databases.createEnumAttribute(db, 
+                    voteCollection,"VoteStatus", 
+                    ["upvoted","downvoted"], 
+                    true
+                ),
+                databases.createStringAttribute(db,voteCollection,"voteById",50,true),
+            ]); 
+            console.log("Vote Attribute created"); 
+        }
+    
